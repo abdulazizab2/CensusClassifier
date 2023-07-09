8c3cc6df7659b90892770ea0ae19164d3d3baf48
abdulaziz <albuainaina8@gmail.com>
2023-07-09 03:00:40 +0300

    add conftest for obtaining pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import joblib
+import numpy as np
+import pandas as pd
+import pytest
+from sklearn.model_selection import train_test_split
+from CensusClassifier.constants import CAT_FEATURES
+
+
+@pytest.fixture(scope="session")
+def data():
+    data = pd.read_csv("./data/cleaned_df.csv")[0:100]  # take few samples for testing
+    train, test = train_test_split(data, test_size=0.20)
+    return train, test
+
+
+@pytest.fixture(scope="session")
+def splitted_data(data):
+    train, _ = data  # does not matter which split for unit tests
+    encoder = joblib.load("./model/categorical_encoder.joblib")
+    y = train["salary"]
+    X = train.drop(["salary"], axis=1)
+    X_categorical = X[CAT_FEATURES].values
+    X_continuous = X.drop(*[CAT_FEATURES], axis=1)
+    X_categorical = encoder.transform(X_categorical)
+
+    X = np.concatenate([X_continuous, X_categorical], axis=1)
+    return X, y

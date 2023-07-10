abaf9c75af28790784a62d81d81b25141252ae01
abdulaziz <albuainaina8@gmail.com>
2023-07-10 05:05:03 +0300

    update build dependencies

--- .render/build.sh
+++ .render/build.sh
@@ -1,3 +1,5 @@
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 dvc remote modify remote-gdrive-github-actions --local \
       gdrive_user_credentials_file /etc/secrets/dvc-gdrive.json
+dvc pull --remote remote-gdrive-github-actions

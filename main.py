d194e7dcca430e5dc11180ec0e0b1896485c9e11
abdulaziz <albuainaina8@gmail.com>
2023-07-10 04:06:03 +0300

    add FastAPI for model prediction and its unit tests

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
-# Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from CensusClassifier.constants import CAT_FEATURES
+from CensusClassifier.ml.data import process_data
+from CensusClassifier.ml.model import inference, load_model
+from CensusClassifier.utils.logger import logging
+import numpy as np
+import pandas as pd
+import json
+
+app = FastAPI()
+
+model = None
+encoder = None
+
+
+class ModelInput(BaseModel):
+    age: int = Field(example=39)
+    workclass: str = Field(example="State-gov")
+    fnlgt: int = Field(example=77516)
+    education: str = Field(example="Bachelors")
+    education_num: int = Field(example=13)
+    marital_status: str = Field(example="Never-married")
+    occupation: str = Field(example="Adm-clerical")
+    relationship: str = Field(example="Not-in-family")
+    race: str = Field(example="White")
+    sex: str = Field(example="Male")
+    capital_gain: int = Field(example=2174)
+    capital_loss: int = Field(example=0)
+    hours_per_week: int = Field(example=40)
+    native_country: str = Field(example="United-States")
+
+
+@app.on_event("startup")
+def load_models():
+    global model, encoder
+    model = load_model("./model/census-rf.joblib")
+    encoder = load_model("./model/categorical_encoder.joblib")
+    logging.info("Model and Encoder are loaded")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello CensusClassifier!"}
+
+
+@app.post("/predict")
+async def predict(data: ModelInput):
+    data_dict = data.dict()
+    data_dict = {key.replace("_", "-"): value for key, value in data_dict.items()}
+    data_df = pd.DataFrame([data_dict])
+    data_df, _, _, _ = process_data(
+        data_df, CAT_FEATURES, training=False, encoder=encoder
+    )
+    result = inference(model=model, X=data_df)
+    result = "<=50K" if result.item() == 0 else ">50K"
+    logging.info(f"Request received: Predction: {result}")
+    return {"prediction": result}
---
+++ tests/test_main.py
@@ -0,0 +1,62 @@
+from fastapi.testclient import TestClient
+from main import app, load_models
+
+client = TestClient(app)
+load_models()
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello CensusClassifier!"}
+
+
+def test_predict_over_50K():
+    data = {
+        "age": 31,
+        "workclass": "Private",
+        "fnlgt": 45781,
+        "education": "Masters",
+        "education_num": 14,
+        "marital_status": "Never-married",
+        "occupation": "Prof-specialty",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 14084,
+        "capital_loss": 0,
+        "hours_per_week": 50,
+        "native_country": "United-States",
+    }
+
+    response = client.post("/predict", json=data)
+    assert response.json() == {"prediction": ">50K"}
+
+
+def test_predict_under_or_equal_50K():
+    data = {
+        "age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital_gain": 2174,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "United-States",
+    }
+
+    response = client.post("/predict", json=data)
+    assert response.json() == {"prediction": ">50K"}
+
+
+def test_wrong_query():
+    data = {"foo": 1}
+
+    response = client.post("/predict", json=data)
+    assert response.status_code == 422
